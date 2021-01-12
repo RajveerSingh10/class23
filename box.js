22b135b431a5989bebd580e6f38ca5dcd40ed378
RajveerSingh10 <75128704+RajveerSingh10@users.noreply.github.com>
2021-01-12 18:48:11 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,22 @@
+class Box{
+  
+  constructor(x,y,width,height){
+    var options={
+      restitution:0.8
+
+    }
+    this.width=width;
+    this.height=height;
+this.body=Bodies.rectangle(x,y,width,height ,options);
+World.add(world,this.body)
+  }
+  display(){
+    push();
+    translate(this.body.position.x,this.body.position.y);
+    rotate(this.body.angle);
+    rectMode(CENTER);
+    fill(255);
+    rect(0,0,this.width,this.height);
+    pop();
+  }
+}
